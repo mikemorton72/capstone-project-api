56ce8bb2e2b638034e104941b9cce0af9dc0769d
Mike Morton <mmorton7295@gmail.com>
2021-07-21 16:05:34 -0500

    creates Follow table so users can follow each other, adds follow generation to db:seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
+  has_many :followers, through: :follower_follows
+  has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followees, through: :followee_follows
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@ user_names.each do |user_name|
   )
   user.save
 end
+
+# Follows (has each user follow 6 others)
+users = User.all
+users.each do |user|
+  all_others = []
+  users.each do |another_user|
+    if user != another_user
+      all_others << another_user 
+    end
+  end
+  all_others.shuffle!
+  6.times do |i|
+    Follow.create(followee_id: all_others[i].id, follower_id: user.id)
+  end
+end
