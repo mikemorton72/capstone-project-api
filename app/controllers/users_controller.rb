0fc7187b2a540215e91e80e770bf39818473eacb
Mike Morton <mmorton7295@gmail.com>
2021-08-18 20:06:28 -0500

    changes users controller for pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    limit = 8
+    if params[:page]
+      offset = (params[:page].to_i - 1) * limit
+      @users = User.order(created_at: :desc).offset(offset).limit(limit)
+    else
+      @users = User.order(created_at: :desc).limit(limit)
+    end
     render "index.json.jbuilder"
   end
 
